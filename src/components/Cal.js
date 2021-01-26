e3248229f1719c4bab2aeea5a9cb804f4735a64d
Edgar Montes <jedgar1mx@gmail.com>
2021-01-26 13:42:59 -0500

    Add calendar events.

--- src/components/Cal.js
+++ src/components/Cal.js
@@ -1,10 +1,12 @@
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import moment from 'moment';
 import './Cal.scss';
 export default class Cal {
   constructor(container, _controller) {
     this.calendar = null;
     this.controller = _controller;
+    this.pickups = [];
   }
 
   createCalendar(_app){
@@ -28,19 +30,99 @@ export default class Cal {
     `;
     tempCal.appendChild(calContainer);
     tempCal.prepend(closeBtn);
-    _app.calendar.calendar = new Calendar(calContainer, {
-      plugins: [ dayGridPlugin ]
-    });
     _app.calendar.buildSchedule(_app);
+    console.log(_app.calendar.pickups);
+    _app.calendar.calendar = new Calendar(calContainer, {
+      plugins: [ dayGridPlugin ],
+      eventSources: _app.calendar.pickups
+    });
     _app.calendar.calendar.render();
     document.querySelector('#app .calendar').className = "calendar active";
   }
 
   buildSchedule(_app){
     console.log(_app.schedule);
+    _app.calendar.buildPickUps(_app);
+  }
+
+  buildPickUps(_app){
+    console.log(_app.schedule);
+    let pastDate = _app.schedule.garbage;
+    let latestDate = _app.schedule.garbage;
+    let gList = {
+      events: [
+        {
+          title  : 'Trash',
+          start  : moment(latestDate).format('YYYY-MM-DD'),
+        }
+      ],
+      color: '#cb4d4f',     // an option!
+      textColor: 'white' // an option!
+    };
+    let rList = {
+      events: [
+        {
+          title  : 'Recycle',
+          start  : moment(latestDate).format('YYYY-MM-DD'),
+        }
+      ],
+      color: '#004445',     // an option!
+      textColor: 'white' // an option!
+    };
+    let bList = {
+      events: [
+        {
+          title  : 'Bulk',
+          start  : moment(latestDate).format('YYYY-MM-DD'),
+        }
+      ],
+      color: '#5f355a',     // an option!
+      textColor: 'white' // an option!
+    };
+
+    for (let index = 0; index < 26; index++) {
+      let tempG = {
+        title  : 'Trash',
+        start  : moment(latestDate).add(14,'d').format('YYYY-MM-DD'),
+      };
+      let tempR = {
+        title  : 'Recycle',
+        start  : moment(latestDate).add(14,'d').format('YYYY-MM-DD'),
+      };
+      let tempB = {
+        title  : 'Bulk',
+        start  : moment(latestDate).add(14,'d').format('YYYY-MM-DD'),
+      };
+      let pasttempG = {
+        title  : 'Trash',
+        start  : moment(pastDate).subtract(14,'d').format('YYYY-MM-DD'),
+      };
+      let pasttempR = {
+        title  : 'Recycle',
+        start  : moment(pastDate).subtract(14,'d').format('YYYY-MM-DD'),
+      };
+      let pasttempB = {
+        title  : 'Bulk',
+        start  : moment(pastDate).subtract(14,'d').format('YYYY-MM-DD'),
+      };
+      pastDate = moment(pastDate).subtract(14,'d');
+      latestDate = moment(latestDate).add(14,'d');
+      gList.events.push(tempG);
+      rList.events.push(tempR);
+      bList.events.push(tempB);
+      gList.events.push(pasttempG);
+      rList.events.push(pasttempR);
+      bList.events.push(pasttempB);
+    }
+    _app.calendar.pickups.push(gList);
+    _app.calendar.pickups.push(rList);
+    _app.calendar.pickups.push(bList);
   }
 
   closeCalendar(ev,_calendar){
+    _calendar.calendar.destroy();
+    _calendar.calendar = null;
+    _calendar.pickups.length = 0;
     let tempClass = ev.target.parentNode.parentNode.className;
     tempClass = tempClass.split(' ');
     ev.target.parentNode.parentNode.className = tempClass[0];
